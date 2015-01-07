e032b0986e7ce284665cd9bea7f28c528f0b1a30
Scully87 <scully87.ts@googlemail.com>
2015-01-07 13:20:53 +0000

    added first test, created routes for index

---
+++ spec/features/photos_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'photos' do
+  context 'no photos have been added' do
+    scenario 'should display a prompt to add a photo' do
+      visit '/photos'
+      expect(page).to have_content 'No photos'
+      expect(page).to have_button 'Add a photo'
+    end
+  end
+end
