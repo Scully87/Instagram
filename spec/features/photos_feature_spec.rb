4b04d1c403ad9934944a51ea54fcf2fcfba87152
Scully87 <scully87.ts@googlemail.com>
2015-01-07 17:07:10 +0000

    created test for displaying a photo

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'photos' do
       expect(page).to have_button 'Add a photo'
     end
   end
+	context 'photos have been added' do
+
+	  before do
+	    Photo.create(name: 'Food')
+	  end
+
+	  scenario 'display photos' do
+	    visit '/photos'
+	    expect(page).to have_content('Food')
+	    expect(page).not_to have_content('No photos')
+	  end
+	end
 end
