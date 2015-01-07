25b9b4e674c906c932019f2c60003b11770650b5
Scully87 <scully87.ts@googlemail.com>
2015-01-07 17:23:33 +0000

    created test for user filling out form to add photo

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -8,7 +8,7 @@ feature 'photos' do
       expect(page).to have_button 'Add a photo'
     end
   end
-context 'photos have been added' do
+	context 'photos have been added' do
 
 	  before do
 	    Photo.create(name: 'Food')
@@ -19,5 +19,15 @@ context 'photos have been added' do
 	    expect(page).to have_content('Food')
 	    expect(page).not_to have_content('No photos')
 	  end
+	end
+	context 'creating photos' do
+		scenario 'prompts user to fill out a form, then displays the new photo' do
+		  visit '/photos'
+		  click_button 'Add a photo'
+		  fill_in 'Name', with: 'Food'
+		  click_button 'Create Food'
+		  expect(page).to have_content 'Food'
+		  expect(current_path).to eq '/photos'
+		end
 	end
 end
