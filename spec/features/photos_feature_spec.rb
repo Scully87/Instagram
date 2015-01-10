bf4fc1a13bc82a71b4f5d2eb7cbe69b871bb28e2
Scully87 <scully87.ts@googlemail.com>
2015-01-10 14:37:28 +0000

    moved helper methods into helper files

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -1,23 +1,9 @@
 require 'rails_helper'
+require 'helpers/photos_helper'
+
+include PhotosHelper
 
 feature 'photos' do
-
-  def sign_up
-    visit('/')
-    click_link('Sign up')
-    fill_in('Email', with: 'test@example.com')
-    fill_in('Password', with: 'testtest')
-    fill_in('Password confirmation', with: 'testtest')
-    click_button('Sign up')
-  end
-
-  def create_post
-    sign_up
-    visit('/')
-    click_link('Add a photo')
-    fill_in('Name', with: 'Food')
-    click_button('Create Photo')
-  end
 
   context 'no photos have been added' do
     scenario 'should display a prompt to add a photo' do
