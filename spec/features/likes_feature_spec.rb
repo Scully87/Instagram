a12f6e7ce2c1252b2b51795f101cf9ceaaa1a074
Scully87 <scully87.ts@googlemail.com>
2015-01-10 16:54:18 +0000

    added columns to users

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -9,7 +9,20 @@ feature 'liking photos' do
     create_post
     visit '/photos'
     click_link 'Like'
-    expect(page).to have_content('1 Like')
+    expect(page).to have_content '1 Like'
+  end
+
+  scenario 'users cannot like their own photo', js: true do
+    sign_up
+    create_post
+    expect(page).to_not have_content 'Like'
+  end
+
+  sceanrio 'users can like each photo once', js: true do
+    visit '/'
+    click_link 'Like'
+    click_link 'Like'
+    expect(page).to have_content '1 like'
   end
 
 end
