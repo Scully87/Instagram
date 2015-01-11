b6d583c2c1b25d1008cb86b33e52d13d2e1aca0d
Scully87 <scully87.ts@googlemail.com>
2015-01-11 22:04:29 +0000

    changed likes link to heart, changed create method in lieks controller to not render json

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,11 @@
 class LikesController < ApplicationController
 
-  def index
-    redirect_to photos_path
-  end
-
   def create
     @photo = Photo.find(params[:photo_id])
     like = @photo.likes.new
     like.user_id = current_user.id
     if like.save
-      render json: { new_like_count: @photo.likes.count }
+      redirect_to photos_path
     else
       flash[:notice] = "You already liked this photo."
       redirect_to photos_path 
