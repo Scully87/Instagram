62d5a144a3cd912cc5a3502137090445f96cb8b9
Scully87 <scully87.ts@googlemail.com>
2015-01-11 22:12:10 +0000

    removed functionality for users to not liking their own photo

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -8,36 +8,30 @@ feature 'liking photos' do
   scenario 'is initially at 0 likes' do
     create_post
     visit '/photos'
-    expect(page).to have_content '0 Likes'
+    save_and_open_page
+    expect(page).to have_content '♥ 0'
   end
 
   context 'when signed in' do
-    before {Photo.create name: 'Food'}
     scenario 'a user can like a photo, which updates the photo like count', js: true do
-      sign_up_first_test_user
+      sign_up_and_post
       visit '/photos'
-      click_link 'Like'
-      expect(page).to have_content '1 Like'
+      click_link '♥'
+      expect(page).to have_content '♥ 1'
     end
     scenario 'users can only like each photo once', js: true do
-      sign_up_first_test_user
+      sign_up_and_post
       visit '/photos'
-      click_link 'Like'
-      click_link 'Like'
-      expect(page).to have_content '1 Like'
+      click_link '♥'
+      click_link '♥'
+      expect(page).to have_content '♥ 1'
     end
-  end
-
-  scenario 'users cannot like their own photo', js: true do
-    sign_up_and_post
-    visit '/photos'
-    expect(page).to_not have_link 'Like'
   end
 
   context 'when not signed in' do
     scenario 'users cannot like a photo' do
       visit '/photos'
-      expect(page).to_not have_link 'Like'
+      expect(page).to_not have_link '♥'
     end
   end
 
